b19abaa4232f7fe47c8ac66af8b430017edf4d67
¨lcastro¨ <luis.ramirez.castro@hotmail.com>
2018-05-25 17:56:15 -0600

    Eventos y acciones de tubos

--- pipe.js
+++ pipe.js
@@ -29,4 +29,43 @@ class Pipe {
       this.speed = 6;
   }
 
-  
+  //Eventos
+
+  //El pajaro choca con un tubo
+
+	hits(bird) {
+	    if ((bird.y - bird.r) < this.top || (bird.y + bird.r) > (height - this.bottom)) {
+	      if (bird.x > this.x && bird.x < this.x + this.w) {
+	        return true;
+	      }
+	    }
+	    return false;
+	  }
+
+	//Dibujar tubo
+
+	show() {
+    stroke(255);
+    fill(200);
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+  }
+
+  //Actualizar tubo
+
+  update() {
+    this.x -= this.speed;
+  }
+
+  //Si el tubo se sale de pantalla
+
+  offscreen() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
